dcd7ff47d3a6c002e6afd86084b9532de0910997
SirHexxus <jamesmichaelstacy@gmail.com>
2020-09-04 16:13:58 -0700

    taskbox UI

---
+++ storybook-static/precache-manifest.c688a2829eda6e711264eccca7785c2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "282c4c7f459b163a6d600ca76259f909",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.27b07096eb7d45f8ddb3.bundle.js"
+  },
+  {
+    "url": "runtime~main.27b07096eb7d45f8ddb3.bundle.js"
+  },
+  {
+    "revision": "d96547e357572a957797",
+    "url": "static/css/main.27fd9282.chunk.css"
+  },
+  {
+    "url": "vendors~main.27b07096eb7d45f8ddb3.bundle.js"
+  },
+  {
+    "url": "vendors~main.27b07096eb7d45f8ddb3.bundle.js.LICENSE.txt"
+  }
+]);
